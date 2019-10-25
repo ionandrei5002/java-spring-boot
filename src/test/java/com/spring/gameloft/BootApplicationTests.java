bd04f913e340afb76db460c42da204df176ba7c7
Sebastian Andrei Ion <sebastianandrei.ion@gameloft.com>
2019-10-25 12:11:11 +0300

    first commit

---
+++ src/test/java/com/spring/gameloft/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gameloft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
